5de2a297604a80d518de92a40b138f48ab81e3ee
Aaron Lafferty <alafferty@valorem.com>
2019-01-11 16:05:03 -0600

    update

--- DSC/IISInstall.ps1
+++ DSC/IISInstall.ps1
@@ -1,19 +1,31 @@
 Configuration Main
 {
 
-Param ( [string] $nodeName )
+	Param ( [string] $nodeName )
 
-Import-DscResource -ModuleName PSDesiredStateConfiguration
+	Import-DscResource -ModuleName PSDesiredStateConfiguration
 
-Node $nodeName
- 
-   <# This commented section represents an example configuration that can be updated as required.
-    WindowsFeature WebServerRole #>
-    {
-      WindowsFeature InstallWebServer
-	  }
-		Ensure = "Present"
-		Name = "Web-Server"
-      }
-    }
+	Node $nodeName
+	{
+
+	#Install IIS Server role
+		WindowsFeature IIS 
+		{
+			Ensure = "Present"
+			Name = "Web-Server"
+		}
+
+	#Install ASP Role
+		WindowsFeature AspNet45 
+		{
+			Ensure = "Present"
+			Name = "Web-Asp-Net45"
+		}
+	#Install IIS Management Console		 
+		 WindowsFeature WebManagementConsole
+        {
+			Ensure = "Present"
+			Name = "Web-Mgmt-Console"
+		}	
+	}
 }
